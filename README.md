2debfca9d446e65ac359d7aaa5c931387055d09a
Arthur Edelstein <arthuredelstein@gmail.com>
2013-08-18 00:34:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Voting Queue Simulation Script Usage Instructions
+
+Arthur D. Edelstein, arthuredelstein@gmail.com
+William A. Edelstein, w.edelstein@gmail.com
+
+Source code from [Edelstein, William A., and Arthur D. Edelstein. "Queuing and elections: long lines, DREs and paper ballots." Proceedings of EVT/WOTE 2010](http://bit.ly/T2BEFr)
+
+The accompanying code, (./voteQueueSimulator.py), was used to simulate the queuing of voters at a polling station. The code assumes that during each voting period, voters arrive at the polling station with a fixed number of voting machines (`numMachines`) in a Poisson process (voter arrival has constant probability per unit time). An array of pseudorandomly-generated times (`arrivalTimes`) is created by the function `generateArrivalTimes`.
+
+Next in the function `simulateVoting`, the time at which each voter commences voting (`votingTimes`) and the time at which each voter finishes voting and vacates the voting machine (`departureTimes`) are computed. The voter departs after a fixed time (`machineTime`) has elapsed since voting commenced, i.e. `departureTimes = votingTimes + machineTime`.
+
+When a voter arrives, if a voting machine is available, then by default that voter should begin voting immediately. If however, no machines are available when the voter arrives, then the voter is forced to wait until a voting machine is vacated. Voters will vote in the order that they arrive (and join the queue). According to these rules, each voter is assessed in chronological order and assigned a `votingTime` and `departureTime` by the `queueCode`. 
+
+Polling conditions of interest that may be varied, including the rates of voter arrival by the hour (`hourlyRates`), the time a voter requires to vote at a machine (`machineTime`), the number of voting machines at the polling station (`numMachines`), the number of voters expected per machine (`votersPerMachine`). One may also count the proportion of trials that meet a certain condition, such as a `thresholdWaitingTime`.
+
+The function `plotAFewRuns` may be modified to plot any number of runs with any parameters of the users choice. The function `runHugeBatch` allows large numbers of trials to be run and the statistics collected. Output files are named `voteCombos.txt` and `dat_[machineTime]_[numMachines]_[votersPerMachine].txt`.
+
+This code is written in the Python programming language (version 2.5 was used at the time of publication), and requires the Python packages [scipy](http://scipy.org), [numpy](numpy.org), and [matplotlib](http://matplotlib.org). Additionally, part of the script contains inline C code that requires a C compiler (we used gcc). 
+
+The output files were analyzed and plotted using Excel and plotted with Axum 5.0.
+
---
+++ voteQueueSimulator.py
@@ -0,0 +1,157 @@
+# These packages are available at http://scipy.org and http://matplotlib.sourceforge.net
+from scipy import *
+from pylab import *
+from numpy.random import uniform,poisson
+from scipy.weave import inline
+import time
+
+# Set the rate at which voters arrive in each hour of the day:
+hourlyRates = array([.1,.1,.05,.05,.05,.1,.1,.05,.05,.05,.1,.1,.1])
+
+def generateArrivalTimes(meanTotalVoters,hourlyRates):
+    startTime = 0
+    arrivalTimes = array([])
+    # Go through each hour and generate arrival times:
+    for hourlyRate in hourlyRates:
+        # The number of voters that arrive in this hour is poisson distributed:
+        n = poisson(lam = meanTotalVoters * hourlyRate)
+        # The time at which the voters arrive is uniformly distributed throughout the hour; sort arrivals in order
+        # and concatenate them with arrivals in previous hours:
+        arrivalTimes = hstack((arrivalTimes,sort(uniform(startTime,startTime+60,n))))
+        # The next hour starts 60 min later:
+        startTime += 60
+        
+    return arrivalTimes # The full list of arrival times
+
+# The following is inline C code, requiring the "scipy.weave" module and a gcc compiler:
+queueCode = """
+    for (int i=numMachines+1;i<numArrivals;i++) {
+        // If the ith voter arrives before the (i-numMachines)th voter has departed:
+        if (arrivalTimes[i] < departureTimes[i-numMachines]) {
+            // The ith voter can only start voting when the (i-numMachines)th voter has left.
+            votingTimes[i] = departureTimes[i-numMachines];
+            // The voter leaves after spending machineTime voting.
+            departureTimes[i] = votingTimes[i] + machineTime;
+        }
+    }
+    int k = 0;
+    return_val = 0;
+"""      
+
+def runQueue(numMachines,numArrivals,arrivalTimes,votingTimes,departureTimes,machineTime):
+    q = inline(code=queueCode, force=0, arg_names=['numMachines','numArrivals','arrivalTimes','votingTimes','departureTimes','machineTime'],compiler='gcc')
+
+def simulateVoting(numMachines,machineTime,arrivalTimes):
+    # Time at which ith voter starts voting; will  be corrected for voter pileup (runQueue, below):
+    votingTimes = arrivalTimes.copy()
+    # Time at which ith voter finishes voting and leaves; will be corrected for voter pileup (runQueue, below):
+    departureTimes = votingTimes + machineTime 
+    
+    # Number of voters:
+    numArrivals = len(arrivalTimes) 
+    # Correct votingTimes and departureTimes for effects of the queue:
+    runQueue(numMachines,numArrivals,arrivalTimes,votingTimes,departureTimes,machineTime)
+
+    # If there is time between arriving and voting, that is the time spent waiting:
+    waitLengths = votingTimes - arrivalTimes
+    return votingTimes,departureTimes,waitLengths
+
+
+def runMultipleTrials(numTrials=10000,numMachines=10,machineTime=5,meanTotalVoters=1500,hourlyRates =hourlyRates):
+    # This function is for running a lot of trials with identical parameters.
+    
+    # Initialize results arrays:
+    waitMaxes=[]
+    lastVoterDepartures=[]
+    
+    for trial in range(numTrials):
+        # Indicate progress:
+        if trial % 1000 == 0:
+            print '\t%d out of %d' % (trial,numTrials)
+        # Run a single trial:
+        arrivalTimes = generateArrivalTimes(meanTotalVoters,hourlyRates)
+        votingTimes,departureTimes,waitLengths = simulateVoting(numMachines,machineTime,arrivalTimes)
+        waitMaxes.append(max(waitLengths))
+        lastVoterDepartures.append(departureTimes[-1])
+    
+    # Convert lists to numpy arrays:
+    waitMaxes = array(waitMaxes)
+    lastVoterDepartures = array(lastVoterDepartures)
+    return waitMaxes,lastVoterDepartures
+
+
+def fractionGreaterThan(data,minval):
+    # Compute fraction of elements of "data" that are greater than "minval".
+    return sum(data>minval)/float(len(data))
+
+def saveStats(machineTime,numMachines,votersPerMachine,statData):
+    # Write statistics to a file.
+    f1 = open("dat_%d_%d_%d.txt" % (machineTime,numMachines,votersPerMachine),'w')
+    savetxt(f1,statData,fmt='%g',delimiter='\t')
+    f1.close()
+
+def runHugeBatch():
+    # This function computes 10000 trials with a variety of parameters.
+    
+    # Set up ranges of parameters of interest:
+    machineTimeList = [2, 5, 7, 10, 15]
+    numMachinesList = [2, 5, 10, 15, 20]
+    votersPerMachineList = [50, 100, 150, 200, 300]
+    thresholdWaitingTimeList = [15, 30, 60, 90, 120]
+    numTrials = 10000
+    count=0
+    dataRowList = []
+    
+    # Loop through all possible parameter combinations:
+    for machineTime in machineTimeList:
+        for numMachines in numMachinesList:
+            for votersPerMachine in votersPerMachineList:
+                meanTotalVoters = votersPerMachine * numMachines
+                count +=1
+                print count,machineTime,numMachines,votersPerMachine
+                
+                # Run the 10000 trials and save their results
+                waitMaxes,lastVoterDepatures = runMultipleTrials(numTrials = numTrials, numMachines = numMachines, machineTime = machineTime, meanTotalVoters = meanTotalVoters)
+                saveStats(machineTime,numMachines,votersPerMachine,transpose(vstack((waitMaxes,lastVoterDepatures))))
+                
+                # Compute number of voters who wait longer than a threshold waiting time:
+                angerFrac = [fractionGreaterThan(waitMaxes,t) for t in thresholdWaitingTimeList]
+                
+                # Store various results in a 2D list
+                dataRow = [machineTime,numMachines,votersPerMachine] + angerFrac
+                dataRowList.append(dataRow)
+
+    # Save results in dataRowList:
+    f1 = open('voteCombos.txt','w')
+    f1.write('machineTime\tnumMachines\tvotersPerMachine\t>15\t>30\t>60\t>90\t>120\n')
+    savetxt(f1,dataRowList,fmt='%g',delimiter='\t')
+    f1.close()
+
+
+def plotAFewRuns():
+    # This plots a smal number of trials.
+    numMachines=10
+    machineTime =5
+    meanTotalVoters = 1500
+    thresholdWaitingTime = 60
+    threshCount = 0
+    
+    figure()
+    
+    # Run 5 individual trials and plot them:
+    for i in range(5):
+        arrivalTimes = generateArrivalTimes(meanTotalVoters,hourlyRates)
+        votingTimes,departureTimes,waitLengths = simulateVoting(numMachines,machineTime,arrivalTimes)
+        plot(arrivalTimes,waitLengths)
+
+    xlabel('Arrival time, min')
+    ylabel('Waiting time, min')
+    title('Vote queuing simulation')
+    show()
+
+# Main program:
+plotAFewRuns()
+startTime = time.time()
+runHugeBatch()
+totalTime = time.time()-startTime
+print "Total computer time: %g",totalTime
